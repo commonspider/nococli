e941f68f495806e53dcbb3a919d5d6a72546c321
myusername <commonspider@proton.me>
2025-08-10 18:45:06 +0200

    list-bases and dump-base

---
+++ src/commonspider_nococli/__main__.py
@@ -0,0 +1,42 @@
+import json
+import sys
+from argparse import ArgumentParser
+
+from pandas import DataFrame
+
+from .api import API
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument("-c", "--config", default="noco.json")
+subparsers = arg_parser.add_subparsers(dest="command")
+
+subparsers.add_parser("list-bases")
+
+dump_base_parser = subparsers.add_parser("dump-base")
+dump_base_parser.add_argument("base_id")
+
+args = arg_parser.parse_args()
+
+with open(args.config) as f:
+    config = json.load(f)
+
+api = API(**config)
+
+if args.command == "list-bases":
+    response = api.list_bases()
+    df = DataFrame(response["list"])
+    print(df[["id", "title"]])
+
+elif args.command == "dump-base":
+    data = {}
+    for table in api.list_tables(base_id=args.base_id)["list"]:
+        records = api.list_table_records(table_id=table["id"])["list"]
+        for record in records:
+            del record["CreatedAt"]
+            del record["UpdatedAt"]
+        data[table["title"]] = records
+    print(json.dumps(data, indent=2))
+
+else:
+    print(f"Unrecognized command: {args.command}")
+    sys.exit(1)
---
+++ src/commonspider_nococli/api.py
@@ -0,0 +1,89 @@
+import inspect
+from functools import wraps
+from inspect import Parameter
+from string import Formatter
+from typing import Any, TypedDict
+from urllib.parse import urljoin
+
+from requests import Session
+
+
+def api(method: str, endpoint: str):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(self, **kwargs):
+            return self.request(method, endpoint, defaults | kwargs)
+
+        defaults = {}
+        for name, value in inspect.signature(function).parameters.items():
+            if value.default != Parameter.empty:
+                defaults[name] = value.default
+        return wrapper
+    return decorator
+
+
+class PageInfo(TypedDict):
+    isFirstPage: bool
+    isLastPage: bool
+    page: int
+    pageSize: int
+    totalRows: int
+
+
+class Base(TypedDict):
+    id: str
+    title: str
+
+
+class BasesList(TypedDict):
+    list: list[Base]
+    pageInfo: PageInfo
+
+
+class Table(TypedDict):
+    id: str
+    base_id: str
+    table_name: str
+    title: str
+
+
+class TablesList(TypedDict):
+    list: list[Table]
+
+
+class TableRecords(TypedDict):
+    list: list[dict]
+    pageInfo: PageInfo
+
+
+class API:
+    def __init__(self, base_url: str, api_token: str):
+        self._session = Session()
+        self._base_url = base_url
+        self._api_token = api_token
+
+    def request(self, method: str, endpoint: str, data: dict[str, Any] = None):
+        data = data or {}
+        endpoint = urljoin(self._base_url, endpoint.format(**data))
+        path_vars = [fn for _, fn, _, _ in Formatter().parse(endpoint) if fn is not None]
+        data = {name: value for name, value in data.items() if name not in path_vars}
+        response = self._session.request(
+            method=method,
+            url=endpoint,
+            json=data,
+            headers={"xc-token": self._api_token}
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @api("GET", "/api/v2/meta/bases")
+    def list_bases(self) -> TablesList:
+        ...
+
+    @api("GET", "/api/v2/meta/bases/{base_id}/tables")
+    def list_tables(self, *, base_id: str) -> TablesList:
+        ...
+
+    @api("GET", "/api/v2/tables/{table_id}/records")
+    def list_table_records(self, *, table_id: str) -> TableRecords:
+        ...
